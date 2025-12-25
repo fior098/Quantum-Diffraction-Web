45367ef0d5d88ba7804a64316019dd4efee015d2
vex <omarovfazil098@gmail.com>
2025-12-25 11:44:10 +0300

    com 9

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing dependencies...
-pip install flask numpy scipy matplotlib pillow
-echo.
-echo Starting Diffraction Simulator...
-echo Open browser at http://localhost:5000
-echo.
-python app.py
-pause
